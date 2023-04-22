51838485f2f65bdf444af1587996d8f892cdc386
Brig1tte <korantengbrigitte96@gmail.com>
2023-04-22 01:25:36 -0700

    Advanced Comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Advanced Comb
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int b = '0';
+	int k = '0';
+
+	while (b <= '0')
+	{
+		while (k <= '9')
+		{
+			if (!(b > k) || b == k)
+			{
+				putchar(b);
+				putchar(k);
+				if (b == '8' && k == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			k++;
+		}
+		k = '0';
+		b++;
+	}
+	return (0);
+}
