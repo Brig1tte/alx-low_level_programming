02d567e40a4a2218a1c28685f7a15c3d012153ba
Brig1tte <korantengbrigitte96@gmail.com>
2023-04-21 22:29:11 -0700

    Print Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print Numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		putchar(y + 48);
+	}
+	putchar('\n');
+	return (0);
+}
