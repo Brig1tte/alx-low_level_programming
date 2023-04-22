4c10685e822f7b48926f1749e03fc5646e98303b
Brig1tte <korantengbrigitte96@gmail.com>
2023-04-21 23:14:07 -0700

    Comb It

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Base 16
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	for (y = 97; x <= 102; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
