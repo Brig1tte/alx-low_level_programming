0e524fad43ecff10b57361c103024a6a3bb7922d
Brig1tte <korantengbrigitte96@gmail.com>
2023-05-02 10:34:38 -0700

    Print One Of Two

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string,from the 1st
+ * @str: every other character to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int x;
+	int l = 0;
+	int b = 0;
+	char *c = str;
+	while (*c != '\0')
+	{
+		c++;
+		l++;
+	}
+	b = l -1;
+	for (x = 0; x <= b; x++)
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+	}
+	_putchar('\n');
+}
