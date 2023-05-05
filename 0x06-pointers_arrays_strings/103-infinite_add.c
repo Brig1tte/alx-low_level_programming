a16df55cce62b5fa56d57aa4e190e9189e9d1982
Brig1tte <korantengbrigitte96@gmail.com>
2023-05-04 22:59:55 -0700

    Add Them

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -40,12 +40,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 	while (*(n1 + a) != '\0')
 		a++;
-
 	while (*(n2 + b) != '\0')
 		b++;
 	a--;
 	b--;
-
 	if (b >= size_r || a >= size_r)
 		return (0);
 
@@ -55,18 +53,15 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val1 = 0;
 		else
 			val1 = *(n1 + a) - '0';
-
 		if (b < 0)
 			val2 = 0;
 		else
 			val2 = *(n2 + b) - '0';
 		temp_tot = val1 + val2 + overflow;
-
 		if (temp_tot >= 10)
 			overflow = 1;
 		else
 			overflow = 0;
-
 		if (digits >= (size_r - 1))
 			return (0);
 
@@ -75,7 +70,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		b--;
 		a--;
 	}
-
 	if (digits == size_r)
 		return (0);
 	*(r + digits) = '\0';
