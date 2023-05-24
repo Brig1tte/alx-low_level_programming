4aa6a89bb86d2f7b76ad59d96b4dae3a86b4856e
Brig1tte <korantengbrigitte96@gmail.com>
2023-05-24 06:08:47 -0700

    212

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,11 +5,12 @@
  * @array: array to be used
  * @size: number of elements in the array
  * @cmp: pointer to the function to be used to compare values
+ * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int k;
-	
+
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
