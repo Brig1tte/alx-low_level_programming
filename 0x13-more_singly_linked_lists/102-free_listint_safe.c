297edd32cb42372e3559c477a4040b31c5a32fdc
Brig1tte <korantengbrigitte96@gmail.com>
2023-06-06 11:03:48 -0700

    Free_Listint_Safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function to free a listint_t list
+ * @h: a pointer to the first node
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t leng = 0;
+	int dfrnz;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		dfrnz = *h - (*h)->next;
+		if (dfrnz > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			leng++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			leng++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (leng);
+}
