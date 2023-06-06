ea66e66fbe3d8c222b5e115278588440613b3144
Brig1tte <korantengbrigitte96@gmail.com>
2023-06-06 09:47:06 -0700

    Print_Listint_Safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,76 @@
+#include "lists.h"
+
+size_t looped_listint_len(const listint_t *head);
+
+/**
+ * looped_listint_len - function to count number of unique codes
+ * @head: pointer to the head of the linked list to be checked
+ * Return: number of unique nodes, 0 if the list is not looped
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *hare, *tortoise;
+	size_t nelem = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	hare = (head->next)->next;
+	tortoise = head->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+
+			while (tortoise != hare)
+			{
+				nelem++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nelem++;
+				tortoise = tortoise->next;
+			}
+			return (nelem);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
+}
+
+/**
+ * print_listint_safe - function to print a listint_t linked list
+ * @head: pointer to the head of the linked list
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nelems, indx = 0;
+
+	nelems = looped_listint_len(head);
+
+	if (nelems == 0)
+	{
+		for (; head != NULL; nelems++)
+		{
+			printf("[%p]%d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
+	else
+	{
+		for (indx = 0; indx < nelems; indx++)
+		{
+			printf("[%p]%d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("->[%p]%d\n", (void *)head, head->n);
+	}
+	return (nelems);
+}
