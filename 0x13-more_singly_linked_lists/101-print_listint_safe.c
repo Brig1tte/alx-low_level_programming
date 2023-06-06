1a99840d1a105956595934e07b2428d56aec5945
Brig1tte <korantengbrigitte96@gmail.com>
2023-06-06 10:08:57 -0700

    Print_Listint_Safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -59,7 +59,7 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		for (; head != NULL; nelems++)
 		{
-			printf("[%p]%d\n", (void *)head, head->n);
+			printf("[%p]%d\n",(void *)head, head->n);
 			head = head->next;
 		}
 	}
@@ -67,10 +67,10 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		for (idx = 0; idx < nelems; idx++)
 		{
-			printf("[%p]%d\n", (void *)head, head->n);
+			printf("[%p]%d\n",(void *)head, head->n);
 			head = head->next;
 		}
-		printf("->[%p]%d\n", (void *)head, head->n);
+		printf("->[%p]%d\n",(void *)head, head->n);
 	}
 	return (nelems);
 }
