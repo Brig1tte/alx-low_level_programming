d56d28e628bf79d89d82fe16f6311aee814385d3
Brig1tte <korantengbrigitte96@gmail.com>
2023-06-12 09:05:04 -0700

    Create_File.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	y = write(fd, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (fd == -1 || y == -1)
 		return (-1);
 
 	close(fd);
