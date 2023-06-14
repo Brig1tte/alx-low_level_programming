49e1de20406d68c54d22ec0ba7e5806a345430ab
Brig1tte <korantengbrigitte96@gmail.com>
2023-06-14 00:18:31 -0700

    3-CP.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,34 +2,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char *create_buffer(char *f);
-void close_f(int fd);
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
  * *create_buffer - function to read 1024 bytes at a time for a buffer
- * @f: the file buffer stores chars for
+ * @file: the file buffer stores chars for
  * Return: pointer to the allocated buffer
  */
-char *create_buffer(char *f)
+char *create_buffer(char *file)
 {
-	char buff;
+	char *b;
 
-	buff = malloc(sizeof(char) * 1024);
-	if (buff == NULL)
+	b = malloc(sizeof(char) * 1024);
+	if (b == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 		exit(99);
 	}
-	return (buff);
+	return (b);
 }
 
-
 /**
- * close_f - function to close the file descriptor
+ * close_file - function to close the file descriptor
  * @fd: the file descriptor to close
- * Return: void
  */
-void close_f(int fd)
+void close_file(int fd)
 {
 	int k;
 
@@ -47,45 +45,49 @@ void close_f(int fd)
  * @argv: Array of pointers to the argument
  * @argc: Number of arguments passed
  * Return: 0(Succes)
+ * Description: if the num of args is incorrect, exit(97)
+ *	if file_from can't read or doesn't exist, exit(98)
+ *	if write to file_to fails or can't be created, exit(99)
+ *	if file descriptor(file_from/to) can't close, exit(100)
  */
 int main(int argc, char *argv[])
 {
 	int f_from, f_to, x, y;
-	char buff;
+	char *b;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buff = create_buffer(argv[2]);
+	b = create_buffer(argv[2]);
 	f_from = open(argv[1], O_RDONLY);
-	x = read(f_from, buff, 1024);
+	x = read(f_from, b, 1024);
 	f_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (f_from == -1 || x == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-			free(buff);
+			free(b);
 			exit(98);
 		}
 
-		y = write(f_to, buff, x);
+		y = write(f_to, b, x);
 		if (f_to == -1 || y == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			free(buff);
+			free(b);
 			exit(99);
 		}
 
-		x = read(f_from, buff, 1024);
+		x = read(f_from, b, 1024);
 		f_to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (x > 0);
 
-	free(buff);
-	close_f(f_from);
-	close_f(f_to);
+	free(b);
+	close_file(f_from);
+	close_file(f_to);
 
 	return (0);
 }
