2c02f78a9524b27b6fddde581d3a537287dd6a7f
Brig1tte <korantengbrigitte96@gmail.com>
2023-08-21 18:50:43 -0700

    DYNAMIC_LIBRARIES

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,4 @@
+Dynamic Libraries
+C - Programming
+
+0x18. C - Dynamic libraries
---
+++ 0x18-dynamic_libraries/abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_abs - function computes the absolute value of an integer
+ *@n: print character
+ *Return: 0
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}
