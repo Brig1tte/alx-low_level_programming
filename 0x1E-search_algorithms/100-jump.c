671ba4da15fa0f63bacfb88d61cd86931c48537a
Brig1tte <korantengbrigitte96@gmail.com>
2023-12-12 16:05:03 -0800

    Jump Search Algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,10 +9,10 @@
   * @size: number of elements in the array
   * @value: the value to search for
   * Return: -1 If val is not present or the array is NULL, else,
-  *         the first index where the value is located
+  *			the first index where the value is located
   *
   * Description: Prints a value every time it is compared in the array
-  * 		Uses the square root of the array size as the jump step
+  *		Uses the square root of the array size as the jump step
   */
 int jump_search(int *array, size_t size, int value)
 {
